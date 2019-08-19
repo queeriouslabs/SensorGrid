859126b9baf0b719c9de8c5faa1710e969fcc730
Rebecca Valentine <psygnisfive@yahoo.com>
2019-08-18 19:01:08 -0700

    Adds more logging info.

--- scanner.py
+++ scanner.py
@@ -2,20 +2,19 @@ import socket
 import time
 import sys
 
-socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(1)
 scanner = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 scanner.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 message = 'sensorgrid-scanner-syn'.encode('utf-8') 
 
-scanner.sendto(message, ('<broadcast>', 1337))
-
-print('Scanning for transmitters...')
 start_time = time.time()
 known_transmitters = []
 
 try:
     while time.time() < start_time + 10:
         try:
+            print('Scanning...')
+            scanner.sendto(message, ('<broadcast>', 1337))
             data, addr = scanner.recvfrom(1024)
             if data.decode('utf-8') == 'sensorgrid-transmitter-ack':
                 print('received ack from: %s' % str(addr))
