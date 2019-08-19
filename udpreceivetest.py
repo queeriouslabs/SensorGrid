5f618d5269c2aa87e47a64a4732f633036a560ae
Rebecca Valentine <psygnisfive@yahoo.com>
2019-08-18 17:03:33 -0700

    Tries a different port.

--- udpreceivetest.py
+++ udpreceivetest.py
@@ -2,7 +2,7 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client.bind(("", 37020))
+client.bind(('', 1337))
 while True:
-    data, addr = sock.recvfrom(1024)
+    data, addr = client.recvfrom(1024)
     print("received message: %s"%data)
