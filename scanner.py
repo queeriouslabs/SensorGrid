7143e420af69859ca39b9761f1fa0cdbe010299d
Rebecca Valentine <psygnisfive@yahoo.com>
2019-08-18 18:28:57 -0700

    adds syn/ack

---
+++ scanner.py
@@ -0,0 +1,22 @@
+import socket
+import time
+import sys
+
+socket.setdefaulttimeout(10)
+scanner = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+scanner.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+message = 'sensorgrid-scanner-ack'.encode('utf-8') 
+
+scanner.sendto(message, ('<broadcast>', 1337))
+
+print('Scanning...')
+start_time = time.time()
+known_transmitters = []
+while time.time() < start_time + 10:
+    try:
+        data, addr = scanner.recvfrom(1024)
+        if data.decode('utf-8') == 'sensorgrid-transmitter-ack':
+            print('received ack from: %s' % str(addr))
+            known_transmitters += [addr]
+    except: continue
+    continue
---
+++ transmitter.py
@@ -0,0 +1,11 @@
+import socket
+
+transmitter = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+transmitter.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+transmitter.bind(('', 1337))
+
+while True:
+    data, addr = transmitter.recvfrom(1024)
+    if data.decode('utf-8') == 'sensorgrid-scanner-syn':
+        print('received ping from %s. responding.' % str(addr))
+        transmitter.sendto(b'sensorgrid-transmitter-ack', addr)
