7dc467d33a94d439521ba5f5db3979f5889f51a2
Rebecca Valentine <psygnisfive@yahoo.com>
2019-08-18 17:25:36 -0700

    fixes name bug

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 message = sys.argv[1].encode('utf-8') 
 
-server.sendto(message, ('<broadcast>', 1337))
+client.sendto(message, ('<broadcast>', 1337))
 
 while True:
     data, addr = client.recvfrom(1024)
