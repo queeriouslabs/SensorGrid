e290ee91f75c5503c4f0cdd98fa213439b1abdc1
Rebecca Valentine <psygnisfive@yahoo.com>
2019-08-18 17:09:09 -0700

    Try sending an input.

--- udpreceivetest.py
+++ udpreceivetest.py
@@ -1,8 +1,9 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 client.bind(('', 1337))
+
 while True:
     data, addr = client.recvfrom(1024)
     print("received message: %s"%data)
--- udpsendtest.py
+++ udpsendtest.py
@@ -1,10 +1,9 @@
 import socket
 import time
+import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-message = 'foo'.encode('utf-8') 
-while True:
-    server.sendto(message, ('<broadcast>', 1337))
-    print("message sent!")
-    time.sleep(1)
+message = sys.argv[0].encode('utf-8') 
+
+server.sendto(message, ('<broadcast>', 1337))
