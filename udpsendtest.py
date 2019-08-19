4182c0c69fd0768e95e97ae2cc216f6af330c3b9
Rebecca Valentine <psygnisfive@yahoo.com>
2019-08-18 17:05:50 -0700

    Try removing an extra bind.

--- udpsendtest.py
+++ udpsendtest.py
@@ -3,7 +3,6 @@ import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-server.bind(('', 44444))
 message = 'foo'.encode('utf-8') 
 while True:
     server.sendto(message, ('<broadcast>', 1337))
